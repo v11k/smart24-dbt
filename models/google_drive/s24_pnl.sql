966ce1bdea819f8d2361d553b3261bef6ccb0405
Viktor Likar <likar.viktor@gmail.com>
2024-04-24 13:46:31 +0200

    s24 pnl

---
+++ models/google_drive/s24_pnl.sql
@@ -0,0 +1,60 @@
+select 
+	'Income' as source_type,
+	cast(replace(honap,'.','-') as date) as date,
+	ugyfel as source_name,
+	cast(osszeg as numeric) as amount
+from "google_drive"."Bevetelek"
+
+UNION
+	
+select 
+	'Facebook' as source_type,
+	cast(replace(replace(kelte,'.','-'),' ','') as date) as date,
+	partner as source_name,
+	cast(brutto as numeric) as amount
+from "google_drive"."Facebook"
+
+UNION
+	
+select 
+	'Google' as source_type,
+	cast(replace(replace(kelte,'.','-'),' ','') as date) as date,
+	partner as source_name,
+	cast(brutto as numeric) as amount
+from "google_drive"."Google"
+
+UNION
+	
+select 
+	'Server' as source_type,
+	cast(replace(replace(kelte,'.','-'),' ','') as date) as date,
+	partner as source_name,
+	cast(brutto as numeric) as amount
+from "google_drive"."Rackforest"
+
+UNION
+	
+select 
+	'Server' as source_type,
+	cast(replace(replace(kelte,'.','-'),' ','') as date) as date,
+	partner as source_name,
+	cast(brutto as numeric) as amount
+from "google_drive"."Litvan_server"
+
+UNION
+	
+select 
+	'ETL' as source_type,
+	cast(replace(replace(kelte,'.','-'),' ','') as date) as date,
+	partner as source_name,
+	cast(brutto as numeric) as amount
+from "google_drive"."Supermetrics"
+
+UNION
+	
+select 
+	'ETL' as source_type,
+	cast(replace(replace(kelte,'.','-'),' ','') as date) as date,
+	partner as source_name,
+	cast(brutto as numeric) as amount
+from "google_drive"."Fivetran"
