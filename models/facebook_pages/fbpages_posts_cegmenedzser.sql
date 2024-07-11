cdf003ee765b0cbf63fef7348cdf53cf577e0207
Viktor Likar <likar.viktor@gmail.com>
2024-07-11 16:21:15 +0200

    FBPages fix 2

--- models/facebook_pages/fbpages_posts_cegmenedzser.sql
+++ models/facebook_pages/fbpages_posts_cegmenedzser.sql
@@ -1,7 +1,7 @@
+{{ config(
+    depends_on={'refs': ['fbpages_post_metrics_' ~ get_company_name()]}
+) }}
 {% set company_name = get_company_name() %}
 
-{{ config(
-    depends_on={'refs': ['fbpages_post_metrics_' ~ company_name]}
-) }}
 
 {{ get_fbpages_posts_query(company_name) }}
