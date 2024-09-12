11b818481d972db6fd408044a9d15839dd91291f
Viktor Likar <likar.viktor@gmail.com>
2024-09-12 10:26:26 +0200

    postgres dateadd

--- macros/set_gsc_by_query_query.sql
+++ macros/set_gsc_by_query_query.sql
@@ -20,7 +20,7 @@ LEFT JOIN
     {{ source('google_search_console', 'search_analytics_by_query') }} s_prev
     ON s.site_url = s_prev.site_url
     AND s.search_type = s_prev.search_type
-    AND s.date = DATEADD(s_prev.date, INTERVAL 30 DAY)
+    AND s.date = s_prev.date + INTERVAL '30 days'
 WHERE 
     a.attribute = '{{ company_name }}'
 
