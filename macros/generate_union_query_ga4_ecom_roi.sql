c8bf39625b891e9a595b6568b0fb93444bb27f97
Viktor Likar <likar.viktor@gmail.com>
2024-09-30 18:20:13 +0200

    ga4ecomroi

--- macros/generate_union_query_ga4_ecom_roi.sql
+++ macros/generate_union_query_ga4_ecom_roi.sql
@@ -1,46 +1,42 @@
 {% macro generate_ga4_union_ecom_roi(company_name) %}
-{% set property_ids = get_property_ids_for_company(company_name) %}
-    {% do log('Property IDs for ' ~ company_name ~ ': ' ~ (property_ids | join(', ')), info=True) %}
 
-    {% if property_ids | length == 0 %}
-        {% do log('No property IDs found for company: ' ~ company_name, info=True) %}
-    {% endif %}
+with fbads as (
+	SELECT
+    	"Date" as date,
+    	(regexp_matches("Ad url tags", 'utm_campaign=([^&]+)'))[1] AS utm_campaign,
+    	SUM("Cost") AS cost
+	FROM {{ ref("fbads_ads_" ~ company_name)}}
+	GROUP BY "Date", utm_campaign)
+,
+gads as (
+	SELECT
+		"Date" as date,
+		"Campaign name" as utm_campaign,
+		sum("Cost") as cost
+	from {{ ref("gads_campaigns_" ~ company_name)}}
+	group by 1,2
+),
+ga4_purchase as (
+	SELECT
+		"Date" as date,
+		"Event campaign name" as utm_campaign,
+		sum("Conversions") as purchases,
+		sum("Event value") as purchase_value
+	FROM {{ ref("ga4_conversions_" ~ company_name)}}
+	WHERE "Event name" = 'purchase'
+	GROUP BY 1,2
+)
+SELECT
+	coalesce(ga4.date, gads.date, fbads.date) as date,
+	coalesce(ga4.utm_campaign, gads.utm_campaign, fbads.utm_campaign) as campaign,
+	coalesce(fbads.cost,0) as fbads_cost,
+	coalesce(gads.cost,0) as gads_cost,
+	coalesce(ga4.purchases,0) as purchases,
+	coalesce(ga4.purchase_value,0) as purchase_value
+FROM ga4_purchase ga4
+full outer join fbads on fbads.date = ga4.date
+	and fbads.utm_campaign = ga4.utm_campaign
+full outer join gads on gads.date = ga4.date
+	and gads.utm_campaign = ga4.utm_campaign
 
-    {% set union_queries = [] %}
-    {% for property_id in property_ids %}
-        {% set ecom_items_table_name = 's24_ecom_items_property' ~ property_id %}
-        {% set properties_table = ref('properties_with_attribute') %}
-        {% set query = "
-        SELECT
-            to_date(e.date, 'YYYYMMDD') as \"Date\",
-            pr.account_display_name as \"Account name\",
-            pr.account_id as \"Account ID\",
-            pr.property_display_name as \"GA4 property\",
-            pr.property_id as \"GA4 property ID\",
-            e.\"itemId\" as \"Item ID\",
-            e.\"itemName\" as \"Item name\",
-            e.\"itemBrand\" as \"Item brand\",
-            e.\"itemRevenue\" as \"Item revenue\",
-            e.\"itemsViewed\" as \"Items viewed\",
-            e.\"itemCategory\" as \"Item category\",
-            e.\"itemCategory2\" as \"Item category 2\",
-            e.\"itemCategory3\" as \"Item category 3\",
-            e.\"itemCategory4\" as \"Item category 4\",
-            e.\"itemCategory5\" as \"Item category 5\",
-            e.\"itemsPurchased\" as \"Items purchased\",
-            e.\"itemsCheckedOut\" as \"Items checked out\",
-            e.\"itemsAddedToCart\" as \"Items added to cart\"
-        FROM ga4." ~ ecom_items_table_name ~ " e
-        LEFT JOIN " ~ properties_table ~ " pr ON pr.property_id::text = e.property_id::text
-        WHERE (e.\"itemsViewed\" + e.\"itemsAddedToCart\" + e.\"itemsCheckedOut\" + e.\"itemsPurchased\" + e.\"itemRevenue\") <> 0
-        "
-        %}
-        {% do union_queries.append(query) %}
-        {% do log('Generated query for property ID ' ~ property_id, info=True) %}
-    {% endfor %}
-    {% if union_queries | length == 0 %}
-        {% do log('No queries generated for company: ' ~ company_name, info=True) %}
-    {% else %}
-        {{ union_queries | join(' UNION ALL ') }}
-    {% endif %}
 {% endmacro %}
