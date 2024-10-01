5e746d8295f49a0c9ef979abb00732dad0531a9e
Viktor Likar <likar.viktor@gmail.com>
2024-10-01 21:23:32 +0200

    fb and gads income

--- macros/generate_union_query_ga4_ecom_roi.sql
+++ macros/generate_union_query_ga4_ecom_roi.sql
@@ -33,7 +33,9 @@ SELECT
 	coalesce(fbads.cost,0) as fbads_cost,
 	coalesce(gads.cost,0) as gads_cost,
 	coalesce(ga4.purchases,0) as purchases,
-	coalesce(ga4.purchase_value,0) as purchase_value
+	coalesce(ga4.purchase_value,0) as purchase_value,
+	case when ga4.utm_campaign like '%google%cpc%' then ga4.purchase_value else 0 end as google_purchase_value,
+	case when ga4.utm_campaign like '%facebook%cpc%' then ga4.purchase_value else 0 end as facebok_purchase_value
 FROM ga4_purchase ga4
 full outer join fbads on fbads.date = ga4.date
 	and fbads.utm_campaign = ga4.utm_campaign
